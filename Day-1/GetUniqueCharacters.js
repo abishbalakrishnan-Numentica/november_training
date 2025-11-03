653ae466900671c0860b137a3db9b1e9142d3bcb
abishbalakrishnan-Numentica <abish.balakrishnan@numentica.com>
2025-11-03 16:56:38 +0530

    init

---
+++ Day-1/GetUniqueCharacters.js
@@ -0,0 +1,5 @@
+/***Get Unique Characters**
+Write a function to return all characters that appear only once in a string.
+Example:
+uniqueChars("success");
+Output: "ue"*/
---
+++ Day-1/RangeIn Array.js
@@ -0,0 +1,6 @@
+/***Find Range (Max - Min) in Array**
+Write a function to return the difference between the largest and smallest numbers in an array.
+Example:
+findRange([3, 45, 12, 7, 89]);
+Output: 86
+---*/
